29e94665a1f468fa6c67cc00f84e3ccf08f4b900
Felipe Romao <feliperomaocad@gmail.com>
2019-05-28 20:58:03 -0300

    Implementando logica

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,25 +1,24 @@
 <template>
-    <div class="calculator">
-        <Display value="1000" />
-        <Button label="AC" triple @onClick="clearMemory"/>
-        <Button label="/" operation @onClick="setOperation"/>
-        <Button label="7" @onClick="addDigit"/>
-        <Button label="8" @onClick="addDigit"/>
-        <Button label="9" @onClick="addDigit"/>
-        <Button label="*" operation @onClick="setOperation"/>
-        <Button label="4" @onClick="addDigit"/>
-        <Button label="5" @onClick="addDigit"/>
-        <Button label="6" @onClick="addDigit"/>
-        <Button label="-" operation @onClick="setOperation"/>
-        <Button label="1" @onClick="addDigit"/>
-        <Button label="2" @onClick="addDigit"/>
-        <Button label="3" @onClick="addDigit"/>
-        <Button label="+" operation @onClick="setOperation"/>
-        <Button label="0" double @onClick="addDigit"/>
-        <Button label="."  @onClick="addDigit"/>
-        <Button label="=" operation />
-    </div>
-    
+  <div class="calculator">
+    <Display :value="displayValue"/>
+    <Button label="AC" triple @onClick="clearMemory"/>
+    <Button label="/" operation @onClick="setOperation"/>
+    <Button label="7" @onClick="addDigit"/>
+    <Button label="8" @onClick="addDigit"/>
+    <Button label="9" @onClick="addDigit"/>
+    <Button label="*" operation @onClick="setOperation"/>
+    <Button label="4" @onClick="addDigit"/>
+    <Button label="5" @onClick="addDigit"/>
+    <Button label="6" @onClick="addDigit"/>
+    <Button label="-" operation @onClick="setOperation"/>
+    <Button label="1" @onClick="addDigit"/>
+    <Button label="2" @onClick="addDigit"/>
+    <Button label="3" @onClick="addDigit"/>
+    <Button label="+" operation @onClick="setOperation"/>
+    <Button label="0" double @onClick="addDigit"/>
+    <Button label="." @onClick="addDigit"/>
+    <Button label="=" operation/>
+  </div>
 </template>
 
 <script>
@@ -28,29 +27,73 @@ import Button from '../components/Button'
 
 export default {
     components: {Button, Display},
+    data: () => {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            value: [0, 0],
+            current: 0
+        }
+    },
     methods: {
         clearMemory(){
-            console.log('Limpar memoria...')
+            Object.assign(this.$data, this.$options.data())
         },
         setOperation(operation){
-            console.log('Operacao ' + operation)
+            if(this.current === 0){
+                this.operation = operation
+                this.current = 1
+            }else{
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try{
+                    this.value[0] = eval(
+                        `${this.value[0]} ${currentOperation} ${this.value[1]}`
+                    )
+                }catch (e){
+                    this.$emit('onError', e)
+                }
+
+                this.value[1] = 0
+                this.displayValue = this.value[0]
+                this.operation =  equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
         addDigit(n){
-            console.log('Digito ' + n)
+            if( n ==="." && this.displayValue.includes(".") ){
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            if (n !== "."){
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.value[i] = newValue
+            }
         }
     }
 }
 </script>
 
 <style>
-.calculator{
-    height: 320px;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
